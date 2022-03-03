59fd70fb97cd9d74e31d213106642f73d22269ab
vitor <vitor-hugo710@hotmail.com>
2022-03-02 22:22:32 -0300

    Exercicio tres

---
+++ aula02-09-2022/exercicio.tres/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio.tres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a distancia percorrida" + " em Km: ");
+            decimal.TryParse(Console.ReadLine(), out decimal kmPercorrido);
+
+            Console.WriteLine("Informe o combustivel gasto" + " em litros: ");
+            decimal.TryParse(Console.ReadLine(), out decimal litrosGastos);
+
+            Console.WriteLine($"sua media foi de {kmPercorrido / litrosGastos}");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
